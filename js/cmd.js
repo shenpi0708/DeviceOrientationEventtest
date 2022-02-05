6c8d9fb487d00e27b9f945cd40b912d91a641b74
shenpi0708 <59052374+shenpi0708@users.noreply.github.com>
2022-02-05 20:57:58 +0800

    test4

--- js/cmd.js
+++ js/cmd.js
@@ -1,28 +1,3 @@
-function clickk() {
-    if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
-        DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            // handle data
-            alert('granted')
-          } else {
-			// handle denied
-            alert('err')
-          }
-        })
-        .catch((err) => {
-            console.log(err)
-        });
-    } else {
-      // han
-      alert('err2')
-    }
-  }
-
-
-
-
-
 if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
 DeviceOrientationEvent.requestPermission()
 .then(permissionState => {
@@ -47,9 +22,16 @@ if (permissionState == 'granted') {
 )
 .catch(console.error);
 } else {
-// handle regular non iOS 13+ devices
-alert('err');
+window.addEventListener('deviceorientation', function(event) {
+    var a = document.getElementById('alpha')
+            b = document.getElementById('beta')
+            g = document.getElementById('gamma')
+            alpha = event.alpha
+            beta = event.beta
+            gamma = event.gamma
+
+    a.innerHTML = Math.round(alpha);
+    b.innerHTML = Math.round(beta);
+    g.innerHTML = Math.round(gamma);
+        }, false);
 }
-
-
-// ZZZz
