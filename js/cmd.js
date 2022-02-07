646ee54dba48ef40b163970c3e2dccc77d63f4b5
shenpi0708 <59052374+shenpi0708@users.noreply.github.com>
2022-02-07 17:09:48 +0800

    Update cmd.js

--- js/cmd.js
+++ js/cmd.js
@@ -37,5 +37,41 @@ window.addEventListener('deviceorientation', function(event) {
     g.innerHTML = Math.round(gamma);
         }, false);
 }}
-clickk()
+if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+DeviceOrientationEvent.requestPermission()
+.then(permissionState => {
+alert(permissionState);    
+if (permissionState == 'granted') {
+    
+    window.addEventListener('deviceorientation', function(event) {
+        var a = document.getElementById('alpha')
+                b = document.getElementById('beta')
+                g = document.getElementById('gamma')
+                alpha = event.alpha
+                beta = event.beta
+                gamma = event.gamma
 
+        a.innerHTML = Math.round(alpha);
+        b.innerHTML = Math.round(beta);
+        g.innerHTML = Math.round(gamma);
+            }, false);
+}
+}
+
+)
+.catch(console.error);
+} else {
+alert('not ios');     
+window.addEventListener('deviceorientation', function(event) {
+    var a = document.getElementById('alpha')
+            b = document.getElementById('beta')
+            g = document.getElementById('gamma')
+            alpha = event.alpha
+            beta = event.beta
+            gamma = event.gamma
+
+    a.innerHTML = Math.round(alpha);
+    b.innerHTML = Math.round(beta);
+    g.innerHTML = Math.round(gamma);
+        }, false);
+}
